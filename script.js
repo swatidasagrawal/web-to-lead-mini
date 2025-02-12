4f66a578bea104f935e4b546926d717b7a044b2e
swati <swati.i.das@accenture.com>
2025-02-12 22:04:52 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function beforesubmit(){
-    let outputdate=document.querySelector(".outputdate");
+let captha=false;
+function beforesubmit(event){
+    if(captha){
+        let outputdate=document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log('inputdate value',inputdate.value);//string form need to convert to date
     let formatteddate= new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value=formatteddate;
     console.log('outputdate value',outputdate.value);
+    }
+    else{
+        alert("Please check the reCaptha box.");
+        event.preventDefault();
+    }
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function capthasuccess(){
+    captha=true;
+}
